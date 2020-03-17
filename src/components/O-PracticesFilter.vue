371d2a13a401e19dbb98d2223e35dc8385b23cc2
Philip Double <philip.double@gmail.com>
2020-03-17 17:56:56 -0400

    updated filters

--- src/components/O-PracticesFilter.vue
+++ src/components/O-PracticesFilter.vue
@@ -1,26 +1,63 @@
 <template>
   <div class="o-practices-filter">
-    <span class='o-filter-item'>All</span>
-    <span class='o-filter-item'>Methodology</span>
-    <span class='o-filter-item'>Culture</span>
-    <span class='o-filter-item'>Ceremonies</span>
-    <span class='o-filter-item'>Strategy</span>
-    <span class='o-filter-item'>Design</span>
-    <span class='o-filter-item'>Management</span>
-    <span class='o-filter-item'>Development</span>
+    <div class="o-practices-filter-large">
+      <span
+        class='o-filter-item'
+        v-for="(type, index) in practiceTypes"
+        :key="`type-${index}`"
+      >
+        {{type}}
+      </span>
+    </div>
+    <v-select
+      :items="practiceTypes"
+      label="Filter Practices"
+      class="o-practices-filter-small"
+    ></v-select>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'O-PracticesFilter',
+  data: () => ({
+    practiceTypes: [
+      'All',
+      'Methodology',
+      'Culture',
+      'Ceremonies',
+      'Strategy',
+      'Design',
+      'Management',
+      'Development',
+    ],
+  }),
 };
 </script>
 
 <style lang="scss" scoped>
-.o-practices-filter{
+.o-practices-filter-large{
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  flex-wrap: wrap;
 }
 
+.o-practices-filter-small {
+    display: none;
+  }
+
 .o-filter-item {
+  padding-left: 5px;
+  padding-right: 5px;
+}
+
+@media screen and (max-width: 375px) {
+  .o-practices-filter-large {
+    display: none !important;
+  }
+  .o-practices-filter-small {
+    display: flex;
+  }
 }
 </style>
