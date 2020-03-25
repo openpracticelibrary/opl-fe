e6e4af7ae622ff146fa1eb9a26407f2663532c74
Shahein Moussavi <shaheinm@gmail.com>
2020-03-25 10:36:18 -0400

    made test work just for ci/cd

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import OCommentCount from '@/components/O-CommentCount.vue';
 
-describe('HelloWorld.vue', () => {
+describe('O-CommentCount.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
+    const count = 2;
+    const wrapper = shallowMount(OCommentCount, {
+      propsData: { count },
     });
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.text()).toContain(count);
   });
 });
