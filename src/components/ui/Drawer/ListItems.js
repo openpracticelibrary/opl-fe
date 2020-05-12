b0b13b6f3076bd82beb789f47e040403cc656b4a
Jim Barton <jibarton@redhat.com>
2020-05-12 19:58:29 -0400

    removed icons from drawer, added hamburger icon when drawer is open

--- src/components/ui/Drawer/ListItems.js
+++ src/components/ui/Drawer/ListItems.js
@@ -3,17 +3,11 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import PeopleIcon from '@material-ui/icons/People';
-import LayersIcon from '@material-ui/icons/Layers';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import Collapse from '@material-ui/core/Collapse';
 import List from '@material-ui/core/List';
 import {makeStyles} from '@material-ui/core/styles';
-import TouchAppIcon from '@material-ui/icons/TouchApp';
-import MobiusLoopIcon from '@material-ui/icons/AllInclusive';
-import ContributeIcon from '@material-ui/icons/AddToPhotos';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -38,21 +32,15 @@ export function TopListItems() {
         <List className={classes.root}>
             <ListSubheader inset>PRACTICES</ListSubheader>
             <ListItem button>
-                <ListItemIcon>
-                    <TouchAppIcon/>
-                </ListItemIcon>
+                <ListItemIcon />
                 <ListItemText primary="Please guide me"/>
             </ListItem>
             <ListItem button>
-                <ListItemIcon>
-                    <LayersIcon/>
-                </ListItemIcon>
+                <ListItemIcon />
                 <ListItemText primary="See everything"/>
             </ListItem>
             <ListItem button onClick={handleClick}>
-                <ListItemIcon>
-                    <MobiusLoopIcon/>
-                </ListItemIcon>
+                <ListItemIcon />
                 <ListItemText primary="By mobius loop"/>
                 {open ? <ExpandLess/> : <ExpandMore/>}
             </ListItem>
@@ -84,21 +72,15 @@ export function BottomListItems() {
         <List className={classes.root}>
             <ListSubheader inset>COMMUNITY</ListSubheader>
             <ListItem button>
-                <ListItemIcon>
-                    <PeopleIcon/>
-                </ListItemIcon>
+                <ListItemIcon />
                 <ListItemText primary="Find a mentor"/>
             </ListItem>
             <ListItem button>
-                <ListItemIcon>
-                    <ContributeIcon/>
-                </ListItemIcon>
+                <ListItemIcon />
                 <ListItemText primary="Become a contributor"/>
             </ListItem>
             <ListItem button>
-                <ListItemIcon>
-                    <DashboardIcon/>
-                </ListItemIcon>
+                <ListItemIcon />
                 <ListItemText primary="Latest happenings"/>
             </ListItem>
         </List>
--- src/components/ui/Drawer/index.js
+++ src/components/ui/Drawer/index.js
@@ -131,7 +131,7 @@ export default function OPLDrawer() {
             >
                 <div className={classes.toolbar}>
                     <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'rtl' ? <ChevronRightIcon/> : <ChevronLeftIcon/>}
+                        <MenuIcon />
                     </IconButton>
                 </div>
 
