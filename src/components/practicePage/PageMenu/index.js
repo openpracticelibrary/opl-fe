c2295ec861bf54cbe2bf704b8129dc7c813c6891
Venus Izadi <mizadi@redhat.com>
2020-05-14 12:32:31 -0400

    adding a menu to practice page

---
+++ src/components/practicePage/PageMenu/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {makeStyles} from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles((theme) => ({
+
+    toolbarSecondary: {
+        
+        overflowX: 'auto',
+    },
+    toolbarLink: {
+        padding: theme.spacing(1),
+        flexShrink: 0,
+        color: theme.palette.text.secondary,
+    },
+}));
+
+export default function PageMenu(props) {
+    const classes = useStyles();
+    const {sections} = props;
+
+    return (
+        <React.Fragment>
+            <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
+                {sections.map((section) => (
+                    <Link
+                        href={section.url}
+                        className={classes.toolbarLink}
+                        variant={'overline'}
+                    >
+                        {section.title}
+                    </Link>
+                ))}
+            </Toolbar>
+        </React.Fragment>
+    );
+}
+
+PageMenu.propTypes = {
+    sections: PropTypes.array,
+    title: PropTypes.string,
+};
--- src/pages/PracticePageContent.js
+++ src/pages/PracticePageContent.js
@@ -2,23 +2,48 @@ import React from 'react';
 import PracticeHeader from '../components/practicePage/Header/index';
 import {makeStyles} from "@material-ui/core/styles/index";
 import Box from '@material-ui/core/Box';
+import PageMenu from '../components/practicePage/PageMenu';
+import Divider from '@material-ui/core/Divider';
 
 const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        flexGrow: 1,
+        padding: theme.spacing(1),
+    },
     titleBox: {
-        display:"flex",
-        justifyContent:"flex-start",
+        display: "flex",
+        justifyContent: "flex-start",
         backgroundColor: theme.palette.common.white,
         width: '100%',
-    },
-
+    }
 }));
+
+const sections = [
+    {title: 'What is it', url: '#'},
+    {title: 'Media', url: '#'},
+    {title: 'Why do it', url: '#'},
+    {title: 'How to', url: '#'},
+    {title: 'Resources', url: '#'},
+    {title: 'AMA', url: '#'},
+];
 
 export default function PracticePageContent() {
     const classes = useStyles();
 
     return (
-        <Box className={classes.titleBox}>
-            <PracticeHeader />
-        </Box>
+        <React.Fragment>
+            <Box className={classes.root}>
+                <Box className={classes.titleBox}>
+                    <PracticeHeader/>
+                </Box>
+                <Box>
+                    <Divider/>
+                    <PageMenu sections={sections}/>
+                    <Divider/>
+                </Box>
+            </Box>
+        </React.Fragment>
     )
 };
