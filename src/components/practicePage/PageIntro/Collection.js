d22294cb75ef29160c85bc6c2f10a0a4816d258f
Venus Izadi <mizadi@redhat.com>
2020-05-20 23:06:06 -0400

    fix spacing eslint

--- src/components/practicePage/PageIntro/Collection.js
+++ src/components/practicePage/PageIntro/Collection.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles/index";
+import {makeStyles} from "@material-ui/core/styles/index";
 import CollectionItem from "./CollectionItem";
 import Grid from "@material-ui/core/Grid";
-import { AmaIcon, CameraIcon, FilledHeartIcon } from "../../../assets/icons";
+import {AmaIcon, CameraIcon, FilledHeartIcon} from "../../../assets/icons";
 import IconButton from '@material-ui/core/IconButton';
-import { useMutation } from "@apollo/react-hooks/lib/index";
-import { LIKE_PRACTICE } from "../../../graphql/";
+import {useMutation} from "@apollo/react-hooks/lib/index";
+import {LIKE_PRACTICE} from "../../../graphql/";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -33,38 +33,38 @@ export default function Collection(props) {
   };
 
   return (
-      <>
-        <Grid
-            container
-            direction="row"
-            justify="flex-start"
-            alignItems="center"
-            className={classes.root}
-        >
-          <Grid item>
-            <Typography variant="overline">Collection</Typography>
-          </Grid>
-          <Grid item>
-            <IconButton onClick={handleLike} className={classes.button}>
-              <CollectionItem amount={props.upvotes}>
-                <FilledHeartIcon/>
-              </CollectionItem>
-            </IconButton>
-          </Grid>
-          <Grid item>
-            <IconButton disabled>
-              <CollectionItem amount={props.imgCount}>
-                <CameraIcon/>
-              </CollectionItem>
-            </IconButton>
-          </Grid>
-          <Grid item>
-            <IconButton disabled>
-              <CollectionItem amount={props.questions}>
-                <AmaIcon/>
-              </CollectionItem>
-            </IconButton>
-          </Grid>
+    <>
+      <Grid
+        container
+        direction="row"
+        justify="flex-start"
+        alignItems="center"
+        className={classes.root}
+      >
+        <Grid item>
+          <Typography variant="overline">Collection</Typography>
+        </Grid>
+        <Grid item>
+          <IconButton onClick={handleLike} className={classes.button}>
+            <CollectionItem amount={props.upvotes}>
+              <FilledHeartIcon/>
+            </CollectionItem>
+          </IconButton>
+        </Grid>
+        <Grid item>
+          <IconButton disabled>
+            <CollectionItem amount={props.imgCount}>
+              <CameraIcon/>
+            </CollectionItem>
+          </IconButton>
+        </Grid>
+        <Grid item>
+          <IconButton disabled>
+            <CollectionItem amount={props.questions}>
+              <AmaIcon/>
+            </CollectionItem>
+          </IconButton>
+        </Grid>
       </Grid>
     </>
   );
