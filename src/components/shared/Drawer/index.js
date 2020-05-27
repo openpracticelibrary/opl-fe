002a8fd9e00c5371e82314820b4872f28b30c870
piyushsn <pnarkhed@redhat.com>
2020-05-27 22:03:35 +0100

    fixed drawer-open-icon and added logo in the drawer

--- src/components/shared/Drawer/index.js
+++ src/components/shared/Drawer/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-import Toolbar from "@material-ui/core/Toolbar";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
@@ -81,38 +80,12 @@ export default function OPLDrawer(props) {
   const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
+    setOpen(!open);
   };
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <Box
-        position="fixed"
-        className={clsx(classes.box, {
-          [classes.boxShift]: open,
-        })}
-        color="inherit"
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open,
-            })}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </Box>
-
       <Drawer
         variant="permanent"
         className={clsx(classes.drawer, {
@@ -130,9 +103,10 @@ export default function OPLDrawer(props) {
           <img
             src={oplLogo}
             alt="labs"
+            width="170px"
           />
           <IconButton
-            onClick={handleDrawerClose}
+            onClick={handleDrawerOpen}
             className={clsx(classes.paddedHamburger)}
           >
             <MenuIcon />
