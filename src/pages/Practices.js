ded41531c2c32bee254389a82576a0643d7fd10f
Philip Double <philip.double@gmail.com>
2020-05-28 17:40:21 -0400

    Created a grid for the pane - think that this pane will be standalone component but will get to that later - populated grid with PracticeBaseCard.js - think that this will be changed but it was there.

--- src/pages/Practices.js
+++ src/pages/Practices.js
@@ -2,9 +2,22 @@ import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_PRACTICES } from "../graphql";
 import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles/index";
+import PracticeCard from "../components/shared/PracticeCards/PracticeCardBase";
 
+const useStyles = makeStyles((theme) => ({
+  practicesPane: {
+    display: "flex",
+    flexDirection: "center",
+    paddingLeft: theme.spacing(10),
+    width: "100%",
+    backgroundColor: theme.palette.common.true_white,
+  },
+}));
 
 export default function Practices() {
+  const classes = useStyles();
+
   const { loading, error, data } = useQuery(GET_PRACTICES);
   if (loading) return <p>Loading...</p>;
   if (error) return `Error! ${error}`;
@@ -15,9 +28,16 @@ export default function Practices() {
         container
         direction="row"
         justify="space-around"
+        className={classes.practicesPane}
       >
         {data.practices.map((p) => (
-          <div>{p.title}</div>
+          <PracticeCard
+            key={p.id}
+            practiceId={p.id}
+            practiceTitle={p.title}
+            coverImage={p.coverImage}
+            tags={p.tags}
+          />
         ))}
       </Grid>
     </>
