b28f6e8dca2fd8536e0088ce003e42e3c243cb5d
Philip Double <philip.double@gmail.com>
2020-05-28 19:52:49 -0400

    adding animations and hover states

--- src/components/shared/PracticeCards/PracticeCardBase.js
+++ src/components/shared/PracticeCards/PracticeCardBase.js
@@ -4,11 +4,12 @@ import { Typography, Paper, Box } from "@material-ui/core";
 import { navigate } from "@reach/router";
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 325,
+  practiceItem: {
+    overflow: 'hidden',
+    backgroundColor: '#fff'
   },
   coverImage: {
-    borderRadius: 25,
+    borderRadius: 10,
     height: 181,
     width: 295,
   },
@@ -16,22 +17,46 @@ const useStyles = makeStyles({
 
 export default function PracticeCardBase(props) {
   const classes = useStyles();
-
+  const [hovered, setHovered] = React.useState(false);
+  
   return (
-    <Box onClick={() => navigate(`/practice/${props.slug}`)}>
-      <Paper elevation={0} className={classes.coverImage}>
+    <Box
+      className={classes.practiceItem}
+      onClick={() => navigate(`/practice/${props.slug}`)}
+      onMouseOut={() => setHovered(false)}
+      onMouseOver={() => setHovered(true)}
+      style={{
+        transform: `${hovered ? 'scale(1.25,1.25)' : 'scale(1, 1)'}`,
+        transition: `${hovered ? '0.5s' : '0.5s'}`,
+        zIndex: `${hovered ? '10' : '1'}`,
+        boxShadow: `${hovered ? '0px 2px 3px rgba(0,0,0,.4)' : '0px 0px 0px 0px'}`,
+      }}
+    >
+      <Paper
+        elevation={0}
+        className={classes.coverImage}
+      >
         <img
           alt={props.practiceTitle}
           src={props.coverImage}
           className={classes.coverImage}
         />
       </Paper>
-      <Typography variant={"subtitle2"}>{props.practiceTitle}</Typography>
-      {props.tags.map((t, i) => (
-        <Typography key={i} variant={"overline"}>
-          #{t.tag}&nbsp;
-        </Typography>
-      ))}
+      <Paper 
+        elevation={0}
+        style={{
+          height: `${hovered ? '300px' : '65px'}`,
+          transform: `${hovered ? 'scale(.90, .90)' : 'scale(1, 1)'}`,
+          transition: `${hovered ? '0.5s' : '0.5s'}`,
+        }}
+      >
+        <Typography variant={"subtitle2"}>{props.practiceTitle}</Typography>
+        {props.tags.map((t, i) => (
+          <Typography key={i} variant={"overline"}>
+            #{t.tag}&nbsp;
+          </Typography>
+        ))}
+      </Paper>
     </Box>
   );
 }
