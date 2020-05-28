f1331142c24f2d83b8acf672f7c160f8f90bce70
Shahein Moussavi <shaheinm@gmail.com>
2020-05-28 01:53:49 -0400

    removed unused dep, still WIP

--- src/components/shared/PracticeCards/index.js
+++ src/components/shared/PracticeCards/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import Popover from "@material-ui/core/Popover";
 import PracticeCardDetails from "./PracticeCardDetails";
 import PracticeCardBase from "./PracticeCardBase";
