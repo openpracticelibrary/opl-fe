3df8b53bfd74cd56578973c7a8bc0e622f58ec5b
Philip Double <philip.double@gmail.com>
2020-05-28 23:27:04 -0400

    actually saving my change this time

--- src/components/shared/PracticeCards/__tests__/PracticeCard.js
+++ src/components/shared/PracticeCards/__tests__/PracticeCard.js
@@ -3,7 +3,7 @@ import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { MockedProvider } from "@apollo/react-testing";
 
-import PracticeCard from "../";
+import PracticeCard from "../PracticeCard";
 
 const mockPracticeCardProps = {
   practiceId: "5ec55c99d36579001355faae",
