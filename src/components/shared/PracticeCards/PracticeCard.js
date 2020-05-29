7864253173797a355c1209df0e9cf809da99aa02
Shahein Moussavi <shaheinm@gmail.com>
2020-05-29 10:46:56 -0400

    added card collection to hover card

--- src/components/shared/PracticeCards/PracticeCard.js
+++ src/components/shared/PracticeCards/PracticeCard.js
@@ -2,15 +2,16 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Paper, Box } from "@material-ui/core";
 import { navigate } from "@reach/router";
+import CardCollection from "./CardCollection";
 
 const useStyles = makeStyles({
   practiceItemBound: {
-    overflow: 'visible',
+    overflow: "visible",
     height: 220,
     margin: 10,
   },
   practiceItem: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
   },
   coverImage: {
@@ -23,13 +24,12 @@ const useStyles = makeStyles({
   },
   subtitle: {
     width: 280,
-    fontSize: 12,
-    fontWeight: 'normal',
-    fontStretch: 'normal',
-    fontStyle: 'normal',
+    fontWeight: "normal",
+    fontStretch: "normal",
+    fontStyle: "normal",
     lineHeight: 1.83,
-    letterSpacing: .3,
-    whiteSpace: 'normal',
+    letterSpacing: 0.3,
+    whiteSpace: "normal",
   },
   cardCollection: {
     alignItems: "left",
@@ -39,13 +39,13 @@ const useStyles = makeStyles({
 export default function PracticeCard(props) {
   const classes = useStyles();
   const [hovered, setHovered] = React.useState(false);
-  
+
   return (
     <Box
       className={classes.practiceItemBound}
       style={{
-        zIndex: `${hovered ? '10' : '1'}`,
-        transition: `${hovered ? '0.5s' : '0.5s'}`,
+        zIndex: `${hovered ? "10" : "1"}`,
+        transition: `${hovered ? "0.5s" : "0.5s"}`,
       }}
     >
       <Box
@@ -54,42 +54,47 @@ export default function PracticeCard(props) {
         onMouseOut={() => setHovered(false)}
         onMouseOver={() => setHovered(true)}
         style={{
-          transform: `${hovered ? 'scale(1.2,1.2)' : 'scale(1, 1)'}`,
-          transition: `${hovered ? '0.5s' : '0.5s'}`,
-          zIndex: `${hovered ? '10' : '1'}`,
-          boxShadow: `${hovered ? '1px 2px 0px rgba(0,0,0,.2)' : '0px 0px 0px 0px'}`,
+          transform: `${hovered ? "scale(1.2,1.2)" : "scale(1, 1)"}`,
+          transition: `${hovered ? "0.5s" : "0.5s"}`,
+          zIndex: `${hovered ? "10" : "1"}`,
+          boxShadow: `${
+            hovered ? "1px 2px 0px rgba(0,0,0,.2)" : "0px 0px 0px 0px"
+          }`,
         }}
       >
-        <Paper
-          elevation={0}
-          className={classes.coverImage}
-        >
+        <Paper elevation={0} className={classes.coverImage}>
           <img
             alt={props.practiceTitle}
             src={props.coverImage}
             className={classes.coverImage}
           />
         </Paper>
-        <Paper 
+        <Paper
           elevation={0}
           style={{
-            transform: `${hovered ? 'scale(.925, .925)' : 'scale(1, 1)'}`,
-            transition: `${hovered ? '0.5s' : '0.5s'}`,
+            transform: `${hovered ? "scale(.925, .925)" : "scale(1, 1)"}`,
+            transition: `${hovered ? "0.5s" : "0.5s"}`,
           }}
         >
-          <Typography className={classes.title}>{props.practiceTitle}</Typography>
+          <Typography className={classes.title}>
+            {props.practiceTitle}
+          </Typography>
           {props.tags.map((t, i) => (
             <Typography key={i} variant={"overline"}>
               #{t.tag}&nbsp;
             </Typography>
           ))}
 
-          { hovered ? 
-            <Paper 
-              elevation={0}
-            >
-              <div className={classes.subtitle} >{props.subtitle}</div>
-            </Paper> : null }
+          {hovered ? (
+            <Paper elevation={0}>
+              <Typography variant={"h6"} className={classes.subtitle}>{props.subtitle}</Typography>
+              <CardCollection
+                upvotes={props.upvotes}
+                imgCount={props.mediaGallery}
+                questions={props.ama}
+              />
+            </Paper>
+          ) : null}
         </Paper>
       </Box>
     </Box>
