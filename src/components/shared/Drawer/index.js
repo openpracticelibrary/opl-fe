25ee78bd6ccf7e1302fc8b76bd959beb19dd1953
Haitham Shahin <hshahin@redhat.com>
2020-06-01 14:53:46 -0400

    add link

--- src/components/shared/Drawer/index.js
+++ src/components/shared/Drawer/index.js
@@ -9,6 +9,7 @@ import {BottomListItems, TopListItems} from "./ListItems";
 import Box from "@material-ui/core/Box";
 import Footer from "./Footer";
 import OPLlogo from "../../../assets/icons/open-practice-librar.png";
+import { Link } from "@reach/router";
 
 const drawerWidth = "16.5rem";
 const useStyles = makeStyles((theme) => ({
@@ -99,7 +100,9 @@ export default function OPLDrawer(props) {
         }}
       >
         <div className={classes.toolbar}>
-          <img src={OPLlogo} alt="OPL" width="170px"/>
+          <Link to="/">
+            <img src={OPLlogo} alt="OPL" width="170px"/>
+          </Link>
           <IconButton
             onClick={handleDrawerOpen}
             className={clsx(classes.paddedHamburger)}
