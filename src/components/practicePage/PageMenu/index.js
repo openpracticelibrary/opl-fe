75d8c19ce04e6bce3e11d541e3b7ac19493b5ff9
Shahein Moussavi <shaheinm@gmail.com>
2020-06-05 15:17:10 -0400

    GH-21: in app subnav

--- src/components/practicePage/PageMenu/index.js
+++ src/components/practicePage/PageMenu/index.js
@@ -4,15 +4,34 @@ import {makeStyles} from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     display: "flex",
     flexDirection: "column",
+    justifyContent: "space-evenly",
     padding: theme.spacing(2, 0),
   },
+  wrapBox: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  pageNavButton: {
+    display: "inline-block",
+    borderRadius: "16.5px",
+    "&:focus": {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
   url: {
+    fontSize: "12px",
+    fontWeight: "SemiBold",
+    textAlign: "center",
+    lineHeight: 2.42,
     color: theme.palette.common.brownish_grey,
     paddingRight: theme.spacing(2),
   },
@@ -41,17 +60,21 @@ export default function PageMenu(props) {
     <>
       <Box className={classes.root}>
         <Toolbar component="nav" variant="dense" disableGutters>
-          <Box flexWrap="wrap">
+          <Box classes={classes.wrapBox}>
             {sections.map((section, i) => (
-              <Typography
+              <Button
                 key={i}
                 onClick={() => handleClick(section.ref)}
-                className={classes.url}
-                variant={"overline"}
+                className={classes.pageNavButton}
               >
-                {section.title}
-              </Typography>
-
+                <Typography
+                  align="center"
+                  className={classes.url}
+                  variant={"overline"}
+                >
+                  {section.title}
+                </Typography>
+              </Button>
             ))}
           </Box>
         </Toolbar>
