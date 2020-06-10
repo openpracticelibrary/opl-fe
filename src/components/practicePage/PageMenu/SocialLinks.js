f3f5b7246601dcd63e1eab1d103470ebc25c4f74
piyushsn <pnarkhed@redhat.com>
2020-06-10 14:50:09 +0100

    added like button on nav bar

--- src/components/practicePage/PageMenu/SocialLinks.js
+++ src/components/practicePage/PageMenu/SocialLinks.js
@@ -3,8 +3,12 @@ import Typography from "@material-ui/core/Typography";
 import {makeStyles} from "@material-ui/core/styles";
 import linkedinIcon from "../../../assets/images/li-in-bug.png";
 import { TwitterShareButton, LinkedinShareButton } from "react-share";
-import { TwitterIcon, MoreItemsIcon } from "../../../assets/icons";
+import { TwitterIcon, MoreItemsIcon, FilledHeartIcon } from "../../../assets/icons";
 import Grid from "@material-ui/core/Grid";
+import IconButton from '@material-ui/core/IconButton';
+import {useMutation} from "@apollo/react-hooks";
+import {LIKE_PRACTICE} from "../../../graphql/";
+import CollectionItem from "../PageIntro/CollectionItem";
 
 const useStyles = makeStyles((theme) => ({
   url: {
@@ -14,11 +18,25 @@ const useStyles = makeStyles((theme) => ({
     lineHeight: 2.42,
     color: theme.palette.common.brownish_grey,
   },
+  button: {
+    "&:hover": {
+      backgroundColor: "transparent"
+    }
+  }
 }));
 
 
 export default function SocialLinks(props) {
   const classes = useStyles();
+  const [likePractice] = useMutation(LIKE_PRACTICE);
+
+  const handleLike = () => {
+    const originalLikes = props.upvotes;
+    const newUpvotes = originalLikes + 1;
+    likePractice({
+      variables: {practiceId: props.practiceId, upvotes: newUpvotes},
+    });
+  };
   return (
     <>
       <Grid
@@ -35,6 +53,11 @@ export default function SocialLinks(props) {
           >
             LIKE 
           </Typography>  
+          <IconButton onClick={handleLike} className={classes.button} data-testid={"heartIcon"}>
+            <CollectionItem amount={props.upvotes}>
+              <FilledHeartIcon/>
+            </CollectionItem>
+          </IconButton>
         </Grid>  
         <Grid item>
           <Typography 
