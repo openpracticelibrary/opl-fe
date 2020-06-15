3bffc4a924c0040852be38475b26030fb57b5c70
Haitham Shahin <hshahin@redhat.com>
2020-06-15 16:54:02 -0400

    updated for background color

--- src/components/allPractices/FilterBar/FilterTags.js
+++ src/components/allPractices/FilterBar/FilterTags.js
@@ -1,44 +1,46 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles/index";
-import { Grid, Button } from "@material-ui/core";
+import { Grid, Button, Chip } from "@material-ui/core";
 
-// const useStyles = makeStyles((theme) => ({
-//   practiceNum: {
-//     fontFamily: "Itim",
-//     fontSize: "200px",
-//     fontWeight: "normal",
-//     textAlign: "center",
-//     color: theme.palette.common.black,
-//   },
-// }));
+const useStyles = makeStyles((theme) => ({
+  practiceNum: {
+    fontFamily: "Itim",
+    fontSize: "200px",
+    fontWeight: "normal",
+    textAlign: "center",
+    color: theme.palette.common.black,
+  },
+}));
 
-const Tag = ({ tag, filter }) => {
+const Tag = ({ tag, filter, selectedFilter }) => {
+    //   const classes = useStyles();
+    
   const buttonRef = React.useRef(null);
 
   //need to consider how to show background of default "selected" button
   //thought this could be done using useEffect but not sure how to make backgrounda appear selected
   React.useEffect(() => {
-    // console.log(buttonRef.current);
-    // buttonRef.current.focus();
+    if(tag === selectedFilter) console.log(buttonRef.current.style='background-color: #e9e9e9');
+    return () => buttonRef.current.style='background-color: transparent';
   });
 
   return (
     <Grid item>
-      <Button ref={buttonRef} onClick={() => filter(tag)}>{tag}</Button>
+      <Chip clickable label={tag} ref={buttonRef} onClick={() => filter(tag)}></Chip>
     </Grid>
   );
 };
 
 const FilterTags = (props) => {
-  //   const classes = useStyles();
 
-  const { tags, filter } = props;
+
+  const { tags, selectedFilter, filter } = props;
 
   return (
     <div data-testid="filterTags">
       <Grid container direction="row" justify="center" alignItems="center">
         {tags.map((tag) => (
-          <Tag key={tag} tag={tag} filter={filter}/>
+          <Tag key={tag} tag={tag} selectedFilter={selectedFilter} filter={filter}/>
         ))}
       </Grid>
     </div>
