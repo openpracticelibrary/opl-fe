a2f9edd953444f39cdca710a43718637af2168a7
piyushsn <pnarkhed@redhat.com>
2020-06-26 15:00:36 +0100

    fixed icon for send feedback

--- src/components/shared/Drawer/Footer.js
+++ src/components/shared/Drawer/Footer.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import FooterList from "./FooterList";
 import Grid from "@material-ui/core/Grid";
 import Disclaimer from "./Disclaimer";
-import { BetaIcon, PodIcon } from "../../../assets/icons";
+import { PodIcon, WebLinkIcon } from "../../../assets/icons";
 
 const iconColor = "#1975ff";
 
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const sections = [
-  { title: "SEND FEEDBACK", url: "https://forms.gle/nXCvPEkjx6VdF6Px6", icon: <BetaIcon fill={iconColor} /> },
+  { title: "SEND FEEDBACK", url: "https://forms.gle/nXCvPEkjx6VdF6Px6", icon: <WebLinkIcon fill={iconColor} width="14px" height="15px" /> },
   { title: "PODCAST", url: "https://podcasts.apple.com/us/podcast/open-practice-podcast/id1501715186", icon: <PodIcon width="15px" height="15px" fill={iconColor} /> },
 ];
 
