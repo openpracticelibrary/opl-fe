c2b68c64f05a6974505bea25bc17696dc5b062d7
piyushsn <pnarkhed@redhat.com>
2020-07-01 15:08:52 +0100

    Tests for Practice page

---
+++ cypress/integration/practicepage_spec.js
@@ -0,0 +1,52 @@
+const { wait } = require("@testing-library/react");
+
+describe('My allPractice page Test', () => {
+  it('Verify Home Page', () => {
+    cy.visit(Cypress.env('HOST'));
+  });
+
+  it("Verify Click of Practice Card", () => {
+    cy.visit(Cypress.env("HOST"));
+    const eventStorming = cy.get('[data-testid="practicecardgrid"]').contains("Event Storming");
+    eventStorming.click();
+    cy.url().should("include", "/practice/event-storming");
+  });
+
+  it("Verify Login Button Shows Dialogue", () => {
+    cy.visit(Cypress.env("HOST"));
+    const eventStorming = cy.get('[data-testid="practicecardgrid"]').contains("Event Storming");
+    eventStorming.click();
+    cy.get('[data-testid="loginForm"]').should("not.be.visible");
+    cy.get('[data-testid="loginButton"]').click();
+    cy.get('[data-testid="loginForm"]').should("be.visible");
+  });
+
+  it("Verify Like Button on Practice Page", () => {
+    cy.visit(Cypress.env("HOST"));
+    const eventStorming = cy.get('[data-testid="practicecardgrid"]').contains("Event Storming");
+    eventStorming.click();
+    //like button tests
+  });
+
+  it("Verify Page Menu on Practice Page", () => {
+    cy.visit(Cypress.env("HOST"));
+    const eventStorming = cy.get('[data-testid="practicecardgrid"]').contains("Event Storming");
+    eventStorming.click();
+    cy.get('[data-testid="pageMenu"]').contains("What").click();
+    cy.get('[data-testid="pageMenu"]').contains("Why").click();
+    cy.get('[data-testid="pageMenu"]').contains("How").click();
+    cy.get('[data-testid="pageMenu"]').contains("Look").click();
+    cy.get('[data-testid="pageMenu"]').contains("Links").click();
+    cy.get('[data-testid="pageMenu"]').contains("AMA").click();
+  });
+
+  it("Verify Add Resources Button on Practice Page", () => {
+    cy.visit(Cypress.env("HOST"));
+    const eventStorming = cy.get('[data-testid="practicecardgrid"]').contains("Event Storming");
+    eventStorming.click();
+    cy.get('[data-testid="addResourceForm"]').should("not.be.visible");
+    cy.get('[data-testid="addResourcesButton"]').click();
+    cy.get('[data-testid="addResourceForm"]').should("be.visible");
+  });
+
+})
